a187a0673852be9e83e382e4f05a6dc801317ada
Gaurav Kumar Thakur <gauravkrthakur07@gmail.com>
2023-07-02 09:58:13 +0530

    Initial Commit

---
+++ src/components/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Content = () => {
+    return (
+        <div className='section'>
+            <h1>Section</h1>
+        </div>
+    )
+}
+
+export default Content
