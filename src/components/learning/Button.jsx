2c01badc204f999dbb7d59f4181ad805490793df
Gaurav Kumar Thakur <gauravkrthakur07@gmail.com>
2023-07-02 15:22:03 +0530

    Nesting styles in styled Components and used psudeoclasses

--- src/components/learning/Button.jsx
+++ src/components/learning/Button.jsx
@@ -7,13 +7,26 @@ const StyledButton = styled.button`
     justify-content: center;
     padding: 5px;
     margin: 5px;
+    flex-direction: column;
     border-radius: 15px;
+
+    &:hover {
+        background-color: grey;
+    }
+
+    .subText {
+        font-size: 10px;
+        color: orange;
+    }
 `
 
 
 const Button = (props) => {
     return (
-        <StyledButton primary={props.primary}>{props.text}</StyledButton>
+        <StyledButton primary={props.primary}>
+            {props.text}
+        <p className='subText'>Sub Text</p>
+        </StyledButton>
     )
 }
 
